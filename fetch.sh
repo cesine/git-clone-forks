e929444ecd03e4e15c230052612495a067e138f2
cesine <cesine@yahoo.com>
2020-03-28 15:49:44 -0400

    fetch uptstream

--- fetch.sh
+++ fetch.sh
@@ -7,10 +7,16 @@ upstream=$1
 repo=~/Downloads/temp-repo
 remotes="./forks.txt"
 
-
 echo "Local Repo $repo"
-
 echo "Remotes $remotes"
+
+mkdir $repo
+cd $repo
+git log -1 || git init
+
+git remote add upstream $upstream
+git fetch upstream
+git branch -a
 
 while IFS= read -r line
 do
